79111a2741aa7399e5192e56a03c2153a5809a95
jose <josetorro1999@hotmail.com>
2020-05-26 09:21:03 +0200

    ya esta?

--- src/client/VistaCliente.java
+++ src/client/VistaCliente.java
@@ -485,9 +485,11 @@ class VistaCliente extends JFrame implements ActionListener {
 
                 documentos.add(fileName0); documentos.add(fileName1); documentos.add(fileName2); documentos.add(fileName3); documentos.add(fileName4);
                 documentos.add(fileName5); documentos.add(fileName6); documentos.add(fileName7); documentos.add(fileName8); documentos.add(fileName9);
-
-                client.blindProcessPartial(documentos);
-
+               
+                if( client.blindProcessPartial(documentos)) {
+                    resultado.setText("Documento firmado. Guardado en: ");
+                    resultado2.setText(client.getPathFile());
+                }
                 //Supongo que habra que enviar la lista al cliente para meterle un elemento igual a todos y despues calcular el hash
                 //System.out.println("HOLA");
 
